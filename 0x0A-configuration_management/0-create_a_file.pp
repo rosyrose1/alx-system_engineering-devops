abaed140487d351bc3e1e7f5ee35fc76edce6395
rosyrose1 <rosinaantwi96@gmail.com>
2023-10-27 05:23:55 -0700

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Creating A file in tmp folder named school
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
