7f2c40e7945721b844d20e44424d5e9961e0f45d
rosyrose1 <rosinaantwi96@gmail.com>
2023-10-27 07:16:13 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
+#!/usr/bin/pup
+# Installing flask from pip3
 
-# Using Puppet, install flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
+}
+
+package {'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
 }
